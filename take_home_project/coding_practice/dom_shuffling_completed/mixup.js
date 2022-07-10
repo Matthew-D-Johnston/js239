212eac01c4d22f11e2b3b5d34541ec35a1ecc5fd
Matthew Johnston <mattdjohnston92@gmail.com>
2022-07-10 12:57:59 -0300

    Add js exercise files.

--- take_home_project/coding_practice/dom_shuffling_completed/mixup.js
+++ take_home_project/coding_practice/dom_shuffling_completed/mixup.js
@@ -7,6 +7,14 @@ let h1 = document.querySelector('main > h1');
 header.insertAdjacentElement('afterbegin', h1);
 
 let images = document.querySelectorAll('figure');
-for (let index = 1; index >= 0; index -= 1) {
+
+let babyMopImage = images[0].querySelector('img');
+let chinStickImage = images[1].querySelector('img');
+
+images[0].insertAdjacentElement('afterbegin', chinStickImage);
+images[1].insertAdjacentElement('afterbegin', babyMopImage);
+
+for (let index = 0; index < images.length; index += 1) {
   document.querySelector('article').insertAdjacentElement('beforeend', images[index]);
 }
+
